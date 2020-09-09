e73d0e19ea87840a23d90f76a71e3043843b02ab
jlimadev <jlima.dev@gmail.com>
2020-09-09 11:48:30 -0300

    created sinup page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
+import SignUp from './pages/SignUp';
 
 const App: React.FC = () => {
   return (
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
-import { Container, Content, Background } from './styles';
+import { FiArrowLeft, FiLock, FiMail, FiUser } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
+import { Background, Container, Content } from './styles';
 
-const SignIn: React.FC = () => (
+const SignUp: React.FC = () => (
   <Container>
+    <Background />
     <Content>
       <img src={logoImg} alt="GoBarber" />
 
       <form>
-        <h1>Faça seu logon</h1>
+        <h1>Faça seu cadastro</h1>
 
+        <Input icon={FiUser} name="name" placeholder="Nome" />
         <Input icon={FiMail} name="email" placeholder="E-mail" />
-
         <Input
           icon={FiLock}
           name="password"
@@ -22,18 +23,15 @@ const SignIn: React.FC = () => (
           placeholder="Password"
         />
 
-        <Button type="submit">Entrar</Button>
-
-        <a href="/">Esqueci minha senha</a>
+        <Button type="submit">Cadastrar</Button>
       </form>
 
       <a href="/">
-        <FiLogIn />
-        Criar conta
+        <FiArrowLeft />
+        Voltar para o Logon
       </a>
     </Content>
-    <Background />
   </Container>
 );
 
-export default SignIn;
+export default SignUp;
