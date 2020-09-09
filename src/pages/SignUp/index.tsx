a962851ea39974e244008ed91f3784409449e14c
jlimadev <jlima.dev@gmail.com>
2020-09-09 14:44:47 -0300

    yup validations

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiLock, FiMail, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import { Background, Container, Content } from './styles';
 
 const SignUp: React.FC = () => {
-  const submitHandler = (data: object): void => {
+  const submitHandler = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'Senha de mínimo 6 dígitios'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
     console.log(data);
-  };
+  }, []);
 
   return (
     <Container>
