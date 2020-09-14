9288939651d957f3127efedccbf5a88102983c67
jlimadev <jlima.dev@gmail.com>
2020-09-14 12:18:52 -0300

    private routes

---
+++ src/routes/Route.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  Route as ReactDOMRoute,
+  RouteProps as ReactDomRouteProps,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+interface RouteProps extends ReactDomRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...restProps
+}) => {
+  const { user } = useAuth();
+  const isSigned = !!user;
+
+  return (
+    <ReactDOMRoute
+      {...restProps}
+      render={({ location }) => {
+        return isPrivate === isSigned ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
