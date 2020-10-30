8f54f3e713406f0b9444440ae1c47e19457b143f
jlimadev <jlima.dev@gmail.com>
2020-10-30 17:02:51 -0300

    configured tests to application

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -50,6 +50,7 @@ const Input: React.FC<InputProps> = ({
       isFocused={isFocused}
       isFilled={isFilled}
       isErrored={!!error}
+      data-testid="input-container"
     >
       {Icon && <Icon size={20} />}
       <input
